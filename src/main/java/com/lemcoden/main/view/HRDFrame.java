18b76382c39a82922f2f2f969c63e98c77134869
e-zhiyuan.zhang <WJlcm5ldGVzL>
2023-02-13 17:31:32 +0800

    华容道页面优化

--- src/main/java/com/lemcoden/main/view/HRDFrame.java
+++ src/main/java/com/lemcoden/main/view/HRDFrame.java
@@ -5,10 +5,8 @@ import javax.swing.text.Document;
 import javax.swing.text.Element;
 import javax.swing.text.html.ListView;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class HRDFrame extends Frame {
@@ -20,7 +18,56 @@ public class HRDFrame extends Frame {
         setTitle("华容道暴力破解");
         Panel panel = new Panel();
         TextArea textArea = new TextArea();
-        textArea.setFont(new Font("Serif", Font.PLAIN, 16));
+        textArea.setSize(100, 200);
+        textArea.setFont(new Font("Serif", Font.PLAIN, 30));
+        textArea.setColumns(5);
+        textArea.setRows(6);
+        textArea.addTextListener(new TextListener() {
+            boolean isSet;
+            int lastCaretPosition;
+
+            @Override
+            public void textValueChanged(TextEvent e) {
+                if (isSet) {
+                    textArea.setCaretPosition(lastCaretPosition);
+                    isSet = false;
+                    return;
+                }
+
+
+                switch (e.getID()) {
+                    case TextEvent.TEXT_VALUE_CHANGED:
+                        String oldText = textArea.getText();
+                        String newText = oldText.replace("\n", "");
+
+                        char[] chars = newText.toCharArray();
+
+                        StringBuilder sb = new StringBuilder();
+                        if (chars.length < 25) {
+                            for (int i = 0; i < chars.length; i++) {
+                                if (i % 4 == 3) {
+                                    sb = sb.append(chars[i]).append("\n");
+                                } else {
+                                    sb = sb.append(chars[i]);
+                                }
+                            }
+                        }else{
+                            sb.append(Arrays.copyOf(chars,24));
+                        }
+
+                        newText = sb.toString();
+                        lastCaretPosition = textArea.getCaretPosition();
+
+                        if (newText.length() - oldText.length() == 1) {
+                            lastCaretPosition++;
+                        }
+                        isSet = true;
+                        textArea.setText(newText);
+                        break;
+                }
+            }
+        });
+
         Label label = new Label("请在左边输入框中输入初始棋盘样式");
         Label label1 = new Label("其中，");
         Label label2 = new Label("\n半个破折号—表示横放棋子");
@@ -37,9 +84,9 @@ public class HRDFrame extends Frame {
                 String text = textArea.getText();
                 String concat = null;
 
-                    concat = text.concat("-");
-                if (text.replace("\n","").length() >= 20) return;
-                if (text.replace("\n","").length()%4 == 3) concat = concat.concat("\n");
+                concat = text.concat("-");
+                if (text.replace("\n", "").length() >= 20) return;
+                if (text.replace("\n", "").length() % 4 == 3) concat = concat.concat("\n");
 
                 textArea.setText(concat);
             }
@@ -51,8 +98,8 @@ public class HRDFrame extends Frame {
             public void actionPerformed(ActionEvent e) {
                 String text = textArea.getText();
                 String concat = text.concat("|");
-                if (text.replace("\n","").length() >= 20) return;
-                if (text.replace("\n","").length()%4 == 3) concat = concat.concat("\n");
+                if (text.replace("\n", "").length() >= 20) return;
+                if (text.replace("\n", "").length() % 4 == 3) concat = concat.concat("\n");
                 textArea.setText(concat);
             }
         });
@@ -63,8 +110,8 @@ public class HRDFrame extends Frame {
             public void actionPerformed(ActionEvent e) {
                 String text = textArea.getText();
                 String concat = text.concat("·");
-                if (text.replace("\n","").length() >= 20) return;
-                if (text.replace("\n","").length()%4 == 3) concat = concat.concat("\n");
+                if (text.replace("\n", "").length() >= 20) return;
+                if (text.replace("\n", "").length() % 4 == 3) concat = concat.concat("\n");
                 textArea.setText(concat);
             }
         });
@@ -75,8 +122,8 @@ public class HRDFrame extends Frame {
             public void actionPerformed(ActionEvent e) {
                 String text = textArea.getText();
                 String concat = text.concat("+");
-                if (text.replace("\n","").length() >= 20) return;
-                if (text.replace("\n","").length()%4 == 3) concat = concat.concat("\n");
+                if (text.replace("\n", "").length() >= 20) return;
+                if (text.replace("\n", "").length() % 4 == 3) concat = concat.concat("\n");
                 textArea.setText(concat);
             }
         });
@@ -87,15 +134,14 @@ public class HRDFrame extends Frame {
             public void actionPerformed(ActionEvent e) {
                 String text = textArea.getText();
                 String concat = text.concat("o");
-                if (text.replace("\n","").length() >= 20) return;
-                if (text.replace("\n","").length()%4 == 3) concat = concat.concat("\n");
+                if (text.replace("\n", "").length() >= 20) return;
+                if (text.replace("\n", "").length() % 4 == 3) concat = concat.concat("\n");
                 textArea.setText(concat);
             }
         });
 
 
-
-        textArea.setSize(400,500);
+        textArea.setSize(400, 500);
         panel.add(textArea);
         panel.add(label);
         panel.add(label1);
@@ -106,7 +152,7 @@ public class HRDFrame extends Frame {
         panel.add(label6);
         panel.add(new Label("                     "));
         Panel btnPanel = new Panel();
-        btnPanel.setLayout(new GridLayout(1,5));
+        btnPanel.setLayout(new GridLayout(1, 5));
         btnPanel.add(hBtn);
         btnPanel.add(shuBtn);
         btnPanel.add(dBtn);
